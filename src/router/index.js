b4d27494e99e3a9f7a3ab04661b7120916333f9c
edemdev7 <ekpomachi@gmail.com>
2024-07-20 21:19:06 +0100

    push

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Notes from '@/components/NotesPost.vue';
 import Note from '@/components/NotePost.vue';
-import EditNote from '@/components/EditNote.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,11 +14,6 @@ const router = createRouter({
       path: '/note/:id',
       name: 'Note',
       component: Note
-    },
-    {
-      path: '/edit/:id',
-      name: 'EditNote',
-      component: EditNote
     }
   ]
 })
